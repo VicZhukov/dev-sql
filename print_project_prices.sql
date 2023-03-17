329710e1a084e3048760ecb8b00b01e7f526b8b3
Victor <32028809+VicZhukov@users.noreply.github.com>
2023-03-17 10:20:19 +0200

    Update file print_project_prices.sql

--- print_project_prices.sql
+++ print_project_prices.sql
@@ -1,4 +1,4 @@
-SELECT p.id AS name, SUM(w.salary * TIMESTAMPDIFF(MONTH, p.start_date, p.finish_date)) AS project_cost
+SELECT p.id AS name, SUM(w.salary * TIMESTAMPDIFF(MONTH, p.start_date, p.finish_date)) AS price
 FROM project p
 JOIN project_worker pw ON p.id = pw.project_id
 JOIN worker w ON pw.worker_id = w.id
